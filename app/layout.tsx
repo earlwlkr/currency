15297391c48c8cbe77f353d9c41e15fb8c5a0aca
earlwlkr <mike358015@gmail.com>
2024-04-29 19:33:02 +0700

    PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,19 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Currencies',
-  description: '',
+  title: 'Currency',
+  description: 'Currency PWA application',
+  generator: 'Next.js',
+  manifest: '/manifest.json',
+  keywords: ['nextjs', 'pwa', 'next-pwa', 'currency', 'currencies'],
+  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#fff' }],
+  authors: [{ name: 'earlwlkr' }],
+  viewport:
+    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
+  icons: [
+    { rel: 'apple-touch-icon', url: 'icons/icon-192x192.png' },
+    { rel: 'icon', url: 'icons/icon-192x192.png' },
+  ],
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import NextPWA from 'next-pwa';
 
-export default nextConfig;
+const withPWA = NextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
