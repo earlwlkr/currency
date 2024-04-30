91ba3757362e2ae76cf2ffd39c44423dfd306fa8
earlwlkr <mike358015@gmail.com>
2024-04-30 12:53:30 +0700

    Scroll area

--- components/AddCurrency.tsx
+++ components/AddCurrency.tsx
@@ -6,6 +6,7 @@ import { useAtom } from 'jotai';
 import countryByCurrencyCode from '@/config/country-by-currency-code.json';
 import { currencyListAtom } from '@/lib/atoms';
 import { Input } from '@/components/ui/input';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const AddCurrency = () => {
   const [currencyList, setCurrencyList] = useAtom(currencyListAtom);
@@ -22,8 +23,6 @@ const AddCurrency = () => {
       {({
         getInputProps,
         getItemProps,
-        getLabelProps,
-        getMenuProps,
         isOpen,
         inputValue,
         highlightedIndex,
@@ -31,46 +30,47 @@ const AddCurrency = () => {
         getRootProps,
       }) => (
         <div>
-          {/* <label {...getLabelProps()}>Enter a currency</label> */}
           <div
             className="inline-block mt-4"
             {...getRootProps({}, { suppressRefError: true })}
           >
             <Input placeholder="Add another currency..." {...getInputProps()} />
           </div>
-          <ul {...getMenuProps()}>
-            {isOpen
-              ? countryByCurrencyCode
-                  .filter(
-                    (item) =>
-                      inputValue &&
-                      item.currency_code &&
-                      (item.country
+
+          {isOpen ? (
+            <ScrollArea className="mt-2 h-40 rounded-md border">
+              {countryByCurrencyCode
+                .filter(
+                  (item) =>
+                    inputValue &&
+                    item.currency_code &&
+                    (item.country
+                      .toLowerCase()
+                      .includes(inputValue.toLowerCase()) ||
+                      item.currency_code
                         .toLowerCase()
-                        .includes(inputValue.toLowerCase()) ||
-                        item.currency_code
-                          .toLowerCase()
-                          .includes(inputValue.toLowerCase()))
-                  )
-                  .map((item, index) => (
-                    <li
-                      key={item.country + item.currency_code}
-                      {...getItemProps({
-                        // key: item.currency_code,
-                        index,
-                        item,
-                        style: {
-                          backgroundColor:
-                            highlightedIndex === index ? 'lightgray' : 'white',
-                          fontWeight: selectedItem === item ? 'bold' : 'normal',
-                        },
-                      })}
-                    >
-                      {item.country} - {item.currency_code}
-                    </li>
-                  ))
-              : null}
-          </ul>
+                        .includes(inputValue.toLowerCase()))
+                )
+                .map((item, index) => (
+                  <div
+                    key={item.country + item.currency_code}
+                    className="p-2 m-2 rounded-md"
+                    {...getItemProps({
+                      // key: item.currency_code,
+                      index,
+                      item,
+                      style: {
+                        backgroundColor:
+                          highlightedIndex === index ? 'lightgray' : 'white',
+                        fontWeight: selectedItem === item ? 'bold' : 'normal',
+                      },
+                    })}
+                  >
+                    {item.country} - {item.currency_code}
+                  </div>
+                ))}
+            </ScrollArea>
+          ) : null}
         </div>
       )}
     </Downshift>
