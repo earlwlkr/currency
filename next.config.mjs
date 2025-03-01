1ec8eb12ed3ae7674e7dd2f9510e04fe796b53eb
earlwlkr <mike358015@gmail.com>
2025-03-01 09:25:50 +0700

    Add PWA support with updated dependency and configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,15 @@
-import path from 'path';
-import NextPWA from 'next-pwa';
-import { createRequire } from 'node:module';
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const isDev = process.env.NODE_ENV === 'development';
-
-const withPWA = NextPWA({
-  dest: 'public',
+const withPWA = withPWAInit({
+  dest: "public",
   disable: isDev,
-  // register: true,
-  skipWaiting: true,
 });
-
-const generateAppDirEntry = (entry) => {
-  const require = createRequire(import.meta.url);
-  const packagePath = require.resolve('next-pwa');
-  const packageDirectory = path.dirname(packagePath);
-  const registerJs = path.join(packageDirectory, 'register.js');
-
-  return entry().then((entries) => {
-    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
-    if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
-      if (Array.isArray(entries['main-app'])) {
-        entries['main-app'].unshift(registerJs);
-      } else if (typeof entries['main-app'] === 'string') {
-        entries['main-app'] = [registerJs, entries['main-app']];
-      }
-    }
-
-    return entries;
-  });
-};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
 
   webpack(config) {
-    if (!isDev) {
-      const entry = generateAppDirEntry(config.entry);
-      config.entry = () => entry;
-    }
-
     return config;
   },
 };
